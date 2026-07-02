b76c2bd81737a6a7a4dccd4254bc5397ea5f62cd
jiaxingxing <justinxing002@gmail.com>
2026-07-02 14:31:23 +0800

    Initial Commit

---
+++ 简单日历/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  简单日历
+//
+//  Created by jiaxing 邢 on 2026/7/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
